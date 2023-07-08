1bf696f910600601d7faaa7b225a4b35e89d6795
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-07-08 06:35:34 +0300

    Adding solutions

---
+++ Exceptions and Error Handling - Lab/MoneyTransactions/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace MoneyTransactions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(',', StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<int, double> accounts = new();
+            foreach (var item in input)
+            {
+                string[] accountInfo = item
+                    .Split('-', StringSplitOptions.RemoveEmptyEntries);
+                accounts.Add(int.Parse(accountInfo[0]), double.Parse(accountInfo[1]));
+            }
+            //accounts[1]++;
+
+
+            string[] commandInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            while (commandInfo[0] != "End")
+            {
+                int accountNumber = int.Parse(commandInfo[1]);
+                double moneyStream = double.Parse(commandInfo[2]);
+                switch (commandInfo[0])
+                {
+                    case "Deposit":
+                        try
+                        {
+                            if (accounts.ContainsKey(accountNumber))
+                            {
+
+                                accounts[accountNumber] += moneyStream;
+                                var currentAccount = accounts.FirstOrDefault(x => x.Key == accountNumber);
+                                Console.WriteLine($"Account {currentAccount.Key} has new balance: {currentAccount.Value:f2}");
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Invalid account!");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+
+                        break;
+                    case "Withdraw":
+                        try
+                        {
+                            if (accounts.ContainsKey(accountNumber))
+                            {
+                                if (accounts[accountNumber] > moneyStream)
+                                {
+                                    accounts[accountNumber] -= moneyStream;
+                                    var currentAccount = accounts.FirstOrDefault(x => x.Key == accountNumber);
+                                    Console.WriteLine($"Account {currentAccount.Key} has new balance: {currentAccount.Value:f2}");
+                                }
+                                else
+                                {
+                                    throw new ArgumentException("Insufficient balance!");
+                                }
+                            }
+                            else
+                            {
+                                throw new ArgumentException("Invalid account!");
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+
+                            Console.WriteLine(ex.Message);
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command!");
+                        break;
+                }
+                Console.WriteLine("Enter another command");
+                commandInfo = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            }
+        }
+    }
+}
---
+++ Exceptions and Error Handling - Lab/PlayCatch/Program.cs
@@ -0,0 +1,111 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace PlayCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int exceptionsCounter = 0;
+
+            while (exceptionsCounter < 3)
+            {
+                string[] commandArg = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (commandArg[0])
+                {
+                    case "Replace":
+                        try
+                        {
+                            input[int.Parse(commandArg[1])] = int.Parse(commandArg[2]);
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex is IndexOutOfRangeException)
+                            {
+                                Console.WriteLine("The index does not exist!");
+                                exceptionsCounter++;
+                            }
+                            else if (ex is FormatException)
+                            {
+                                Console.WriteLine("The variable is not in the correct format!");
+                                exceptionsCounter++;
+                            }
+                        }
+                        break;
+                    case "Print":
+                        int startIndex = default;
+                        int endIndex = default;
+
+                        try
+                        {
+                            if (int.TryParse(commandArg[1], out startIndex)
+                            && int.TryParse(commandArg[2], out endIndex)
+                            )
+                            {
+                                if (startIndex >= 0
+                                && startIndex < input.Length
+                                && endIndex >= 0
+                                && endIndex < input.Length
+                                )
+                                {
+                                    var printRange = input.Skip(int.Parse(commandArg[1]))
+                                        .Take(endIndex - startIndex + 1).ToArray();
+                                    Console.WriteLine(String.Join(", ", printRange));
+                                }
+                                else
+                                {
+                                    throw new IndexOutOfRangeException();
+                                }
+                            }
+                            else
+                            {
+                                throw new FormatException();
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex is IndexOutOfRangeException)
+                            {
+                                Console.WriteLine("The index does not exist!");
+                                exceptionsCounter++;
+                            }
+                            else if (ex is FormatException)
+                            {
+                                Console.WriteLine("The variable is not in the correct format!");
+                                exceptionsCounter++;
+                            }
+                        }
+                        break;
+                    case "Show":
+                        try
+                        {
+                            int indexToPrint = int.Parse(commandArg[1]);
+                            Console.WriteLine(input[indexToPrint]);
+                        }
+                        catch (Exception ex)
+                        {
+                            if (ex is IndexOutOfRangeException)
+                            {
+                                Console.WriteLine("The index does not exist!");
+                                exceptionsCounter++;
+                            }
+                            else if (ex is FormatException)
+                            {
+                                Console.WriteLine("The variable is not in the correct format!");
+                                exceptionsCounter++;
+                            }
+                        }
+                        break;
+                }
+            }
+            Console.WriteLine(String.Join(", ", input));
+        }
+    }
+}
