ca08ca7b42a87ae57193bb9f072e4d5274d0ea83
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-07-17 06:06:09 +0300

    Adding solutions to problems

---
+++ 08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/CommandPattern/Core/Engine.cs
@@ -0,0 +1,44 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace CommandPattern.Core
+{
+    public class Engine: IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter commandInterpreter)
+        {
+            this.commandInterpreter = commandInterpreter;
+        }
+
+        public ICommandInterpreter CommandInterpreter
+        {
+            get { return commandInterpreter; }
+        }
+
+        void IEngine.Run()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                try
+                {
+                    string result = commandInterpreter.Read(input);
+
+                    Console.WriteLine(result);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ 08. CSharp-OOP-Reflection-and-Attributes-Exercise-Skeleton_6.0/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValidationAttributes.Attributes
+{
+    public  class MyRangeAttribute : MyValidationAttribute
+    {
+        private int minValue;
+        private int maxValue;
+
+        public MyRangeAttribute(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+        }
+
+        public int MinValue
+        {
+            get { return minValue; }
+            set { minValue = value; }
+        }
+
+
+        public int MaxValue
+        {
+            get { return maxValue; }
+            set { maxValue = value; }
+        }
+
+        public override bool IsValid(object obj)
+      => (int)obj >= minValue && (int)obj < maxValue;
+    }
+}
