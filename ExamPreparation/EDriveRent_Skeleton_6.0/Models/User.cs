3acf33f2da0385c523de9b9bf6d40a79aba10939
kossev89 <122936075+kossev89@users.noreply.github.com>
2023-07-19 07:01:54 +0300

    Adding solutions

---
+++ ExamPreparation/EDriveRent_Skeleton_6.0/Models/User.cs
@@ -0,0 +1,91 @@
+﻿using EDriveRent.Models.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EDriveRent.Models
+{
+    public class User : IUser
+    {
+        private string firstName;
+        private string lastName;
+        private string drivingLicenseNumber;
+
+        public User(string firstName, string lastName, string drivingLicenseNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.drivingLicenseNumber = drivingLicenseNumber;
+            Rating = 0;
+            IsBlocked = false;
+        }
+
+        public string FirstName
+        {
+            get => firstName;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("FirstName cannot be null or whitespace!");
+                }
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("LastName cannot be null or whitespace!");
+                }
+                lastName = value;
+            }
+        }
+
+        public double Rating { get; private set; }
+
+        public string DrivingLicenseNumber
+        {
+            get => drivingLicenseNumber;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Driving license number is required!");
+                }
+                drivingLicenseNumber = value;
+            }
+        }
+
+        public bool IsBlocked { get; private set; }
+
+        public void DecreaseRating()
+        {
+            Rating += 0.5;
+            if (Rating > 10)
+            {
+                Rating = 10;
+            }
+        }
+
+        public void IncreaseRating()
+        {
+            Rating -= 2;
+            if (Rating < 0)
+            {
+                Rating = 0;
+                IsBlocked = true;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} Driving license: {drivingLicenseNumber} Rating: {Rating}";
+        }
+    }
+}
